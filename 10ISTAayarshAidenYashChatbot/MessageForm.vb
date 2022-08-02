54eacc4ce9a50e62ba3c9e5e7f781ba7b8d733ae
B34rly <74686458+B34rly@users.noreply.github.com>
2022-08-02 15:05:35 +1000

    added more chatbot chatting

--- 10ISTAayarshAidenYashChatbot/MessageForm.vb
+++ 10ISTAayarshAidenYashChatbot/MessageForm.vb
@@ -4,8 +4,23 @@ Public Class MessageForm
     Dim WithEvents recogniser As SpeechRecognitionEngine
     Dim speaker As SpeechSynthesizer
     Dim wordOutput As String
+    Dim recentChatBotMessage As String
+
+    Dim goodWords As New List(Of String)({"well", "good", "great", "wonderful", "alright"})
+    Dim badWords As New List(Of String)({"bad", "terrible", "unwell"})
+    Dim isWords As New List(Of String)({"is", "it's"})
+    Dim affirmativeWords As New List(Of String)({"yes", "please", "of course", "affirmative"})
+    Dim refusalWords As New List(Of String)({"no", "not", "never"})
+    Dim systemWords As New List(Of String)({"circulatory", "respiratory", "digestive", "skeletal", "nervous"})
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        For Each word As String In goodWords
+            badWords.Add("not " + word)
+        Next
+        For Each word As String In badWords
+            goodWords.Add("not " + word)
+        Next
+
         recogniser = New SpeechRecognitionEngine(New System.Globalization.CultureInfo("en-US"))
         recogniser.SetInputToDefaultAudioDevice()
         speaker = New SpeechSynthesizer()
@@ -14,6 +29,7 @@ Public Class MessageForm
         recogniser.LoadGrammar(New DictationGrammar("grammar:dictation"))
 
         Spacer.Size = New Drawing.Size(MessagePanel.Size.Width - MessagePanel.Margin.Size.Width, 0)
+        Send_Chatbot_Message("Hi! How are you?")
     End Sub
 
     Private Sub recognized_completed() Handles recogniser.RecognizeCompleted
@@ -37,32 +53,89 @@ Public Class MessageForm
     End Sub
 
     Private Sub Chatbot_Respond(Input As String) 'put all the chatting logic in here :D
-        Dim Response = "placeholder message bro"
+        Dim Response = "Sorry, I didn't get that"
+        If recentChatBotMessage.Contains("How are you") Then
+            For Each word As String In goodWords
+                If Input.Contains(word) Then
+                    Response = "That's great to hear!" & vbNewLine & "Oh I forgot to ask, what's your name?"
+                End If
+            Next
+            For Each word As String In badWords
+                If Input.Contains(word) Then
+                    Response = "I'm sorry to hear that!" & vbNewLine & "Oh I forgot to ask, what's your name?"
+                End If
+            Next
+        ElseIf recentChatBotMessage.Contains("your name?") Then
+            For Each word As String In isWords
+                Dim chatterName As String = Input
+                If Input.Contains(word) Then
+                    chatterName = Input.Split(word)(Input.Split(word).Length - 1)
+                End If
+                Response = chatterName & ", huh? I like that name!"
+                Response = Response & vbNewLine & "Well, " & chatterName & ", do you want to learn about the human body systems?"
+            Next
+        ElseIf recentChatBotMessage.Contains("learn about the hu") Then
+            For Each word As String In refusalWords
+                If Input.Contains(word) Then
+                    Response = "Get the fuck out you troll piece of shit"
+                End If
+            Next
+            For Each word As String In affirmativeWords
+                If Input.Contains(word) Then
+                    Response = "Great!" & vbNewLine & "Which system do you want to hear about first? The nervous, skeletal, digestive, circulatory or respiratory system?"
+                End If
+            Next
+        ElseIf recentChatBotMessage.Contains("hear about firs") Then
+            For Each word As String In systemWords
+                If Input.Contains(word) Then
+                    If word = "circulatory" Then
+                        Response = "Oh the circulatory system, I love this one!" & vbNewLine & "actual information"
+                    ElseIf word = "respiratory" Then
+                        Response = "The respiratory system's really important!" & vbNewLine & "info part 2"
+                    ElseIf word = "nervous" Then
+                        Response = "This one is my favourite, it's really technical!" & vbNewLine & "Oh, did you know the brain, and by extension the nervous system, is the only part of the body to have named itself?" & vbNewLine & "That's really cool, isn't it?" & "As you may have guessed, the nervous system is INFORMATION HERE"
+                    ElseIf word = "skeletal" Then
+                        Response = "The skeletal system is the backbone of the rest of the body!" & vbNewLine & "Haha, did you see what I did there?" & vbNewLine & "Well, the skeletal system INFORMATION HERE"
+                    ElseIf word = "digestive" Then
+                        Response = "The digestive system is easily the crappiest of them all. No I mean literally!" & vbNewLine & "The digestive system involves INFO TIME"
+                    End If
+                End If
+            Next
+        End If
+
         If Input.Contains("yoo now way") Then
             Response = "whatever for now baby ily"
         End If 'yknow actually make this something
+
         Send_Chatbot_Message(Response)
     End Sub
 
-    Private Sub Send_Chatbot_Message(Message)
-        Dim botMsgLbl As New Label
+    Private Sub Send_Chatbot_Message(Message As String)
+        If Not Message.Contains("Sorry, I didn't get that") Then
+            recentChatBotMessage = Message
+        End If
 
-        botMsgLbl.Parent = MessagePanel
-        botMsgLbl.Text = Message
-        botMsgLbl.AutoSize = True
-        botMsgLbl.Dock = DockStyle.Left
-        botMsgLbl.Margin = New Padding(3, 6, 3, 6)
-        botMsgLbl.Padding = New Padding(3)
-        botMsgLbl.Font = New Font("Segoe UI", 10, FontStyle.Regular)
-        botMsgLbl.MaximumSize = New Size(MessagePanel.Size.Width / 3 * 2, 0)
-        botMsgLbl.BackColor = SystemColors.ControlLight
-        botMsgLbl.ForeColor = SystemColors.ControlText
+        For Each individualMessage As String In Message.Split(vbNewLine)
+            Dim botMsgLbl As New Label
+
+            botMsgLbl.Parent = MessagePanel
+            botMsgLbl.Text = individualMessage.Trim()
+            botMsgLbl.AutoSize = True
+            botMsgLbl.Dock = DockStyle.Left
+            botMsgLbl.Margin = New Padding(3, 6, 3, 6)
+            botMsgLbl.Padding = New Padding(3)
+            botMsgLbl.Font = New Font("Segoe UI", 10, FontStyle.Regular)
+            botMsgLbl.MaximumSize = New Size(MessagePanel.Size.Width / 3 * 2, 0)
+            botMsgLbl.BackColor = SystemColors.ControlLight
+            botMsgLbl.ForeColor = SystemColors.ControlText
+        Next
+
 
         If TTS_Enabled.Checked Then 'add mute checkbox
             speaker.SpeakAsyncCancelAll()
             speaker.SpeakAsync(Message)
         End If
-        MessagePanel.ScrollControlIntoView(botMsgLbl)
+        MessagePanel.AutoScrollPosition = New Point(-1)
     End Sub
 
     Private Sub Send_Message()
@@ -90,5 +163,9 @@ Public Class MessageForm
 
     Private Sub InputBox_TextChanged(sender As Object, e As EventArgs) Handles InputBox.TextChanged
         STT_and_Enter.BackgroundImage = If(InputBox.Text = "", My.Resources.record, My.Resources.send)
+    End Sub
+
+    Private Sub HelpButton_Click(sender As Object, e As EventArgs) Handles HelpButton.Click
+        Questions.Show()
     End Sub
 End Class
---
+++ 10ISTAayarshAidenYashChatbot/Questions.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Questions
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.Controls.Add(Me.Label1)
+        Me.FlowLayoutPanel1.Controls.Add(Me.Label2)
+        Me.FlowLayoutPanel1.Controls.Add(Me.Label3)
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(702, 413)
+        Me.FlowLayoutPanel1.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(3, 3)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(3)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(185, 28)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Whatever Whatever"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(3, 37)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(3)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(69, 28)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Label2"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(3, 71)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(3)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(185, 28)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Whatever Whatever"
+        '
+        'Questions
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(702, 413)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Name = "Questions"
+        Me.Text = "Questions"
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+End Class
