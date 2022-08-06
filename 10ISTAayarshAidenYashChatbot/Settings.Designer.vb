0c4d39a4de90a7c15ed3ee1f7c085b1318ae687a
B34rly <74686458+B34rly@users.noreply.github.com>
2022-08-06 18:54:45 +1000

    added settings, need to actually make the form do stuff tho

---
+++ 10ISTAayarshAidenYashChatbot/Settings.Designer.vb
@@ -0,0 +1,69 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Settings
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.CheckedListBox1 = New System.Windows.Forms.CheckedListBox()
+        Me.FlowLayoutPanel1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.Controls.Add(Me.CheckedListBox1)
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(0, 0)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Padding = New System.Windows.Forms.Padding(12)
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(800, 450)
+        Me.FlowLayoutPanel1.TabIndex = 0
+        '
+        'CheckedListBox1
+        '
+        Me.CheckedListBox1.FormattingEnabled = True
+        Me.CheckedListBox1.Items.AddRange(New Object() {"Chatbot Text To Speech", "Message Notifications", "Music"})
+        Me.CheckedListBox1.Location = New System.Drawing.Point(15, 15)
+        Me.CheckedListBox1.MultiColumn = True
+        Me.CheckedListBox1.Name = "CheckedListBox1"
+        Me.CheckedListBox1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.CheckedListBox1.Size = New System.Drawing.Size(479, 280)
+        Me.CheckedListBox1.Sorted = True
+        Me.CheckedListBox1.TabIndex = 0
+        Me.CheckedListBox1.UseTabStops = False
+        '
+        'Settings
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
+        Me.Name = "Settings"
+        Me.Text = "Settings1"
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents CheckedListBox1 As CheckedListBox
+End Class
