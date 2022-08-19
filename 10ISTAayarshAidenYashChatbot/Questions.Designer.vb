455d464541e6a8449f9f65856fc672860f369c47
B34rly <74686458+B34rly@users.noreply.github.com>
2022-08-19 14:31:19 +1000

    finished question form design

--- 10ISTAayarshAidenYashChatbot/Questions.Designer.vb
+++ 10ISTAayarshAidenYashChatbot/Questions.Designer.vb
@@ -23,6 +23,9 @@ Partial Class Questions
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.QuestionsLabel = New System.Windows.Forms.Label()
+        Me.FlowLayoutPanel1 = New System.Windows.Forms.FlowLayoutPanel()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.FlowLayoutPanel1.SuspendLayout()
         Me.SuspendLayout()
         '
         'QuestionsLabel
@@ -38,20 +41,48 @@ Partial Class Questions
         Me.QuestionsLabel.TabIndex = 1
         Me.QuestionsLabel.Text = "Questions"
         '
+        'FlowLayoutPanel1
+        '
+        Me.FlowLayoutPanel1.AutoScroll = True
+        Me.FlowLayoutPanel1.BackColor = System.Drawing.SystemColors.Window
+        Me.FlowLayoutPanel1.Controls.Add(Me.Label1)
+        Me.FlowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.FlowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown
+        Me.FlowLayoutPanel1.Location = New System.Drawing.Point(15, 60)
+        Me.FlowLayoutPanel1.Name = "FlowLayoutPanel1"
+        Me.FlowLayoutPanel1.Padding = New System.Windows.Forms.Padding(15, 15, 30, 15)
+        Me.FlowLayoutPanel1.Size = New System.Drawing.Size(584, 309)
+        Me.FlowLayoutPanel1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(18, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(478, 56)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Add Questioblah blah blah blah blha blhab lhab lhab haoeuns Here"
+        '
         'Questions
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(790, 516)
+        Me.ClientSize = New System.Drawing.Size(614, 384)
+        Me.Controls.Add(Me.FlowLayoutPanel1)
         Me.Controls.Add(Me.QuestionsLabel)
         Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
         Me.Name = "Questions"
         Me.Padding = New System.Windows.Forms.Padding(15, 9, 15, 15)
         Me.Text = "Questions"
+        Me.FlowLayoutPanel1.ResumeLayout(False)
+        Me.FlowLayoutPanel1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents QuestionsLabel As Label
+    Friend WithEvents FlowLayoutPanel1 As FlowLayoutPanel
+    Friend WithEvents Label1 As Label
 End Class
--- 10ISTAayarshAidenYashChatbot/Questions.vb
+++ 10ISTAayarshAidenYashChatbot/Questions.vb
@@ -1,5 +1,2 @@
 ﻿Public Class Questions
-    Private Sub SettingsLabel_Click(sender As Object, e As EventArgs) Handles QuestionsLabel.Click
-
-    End Sub
 End Class
